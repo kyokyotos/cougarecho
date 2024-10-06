f2a269e030dc98668ad1b00558c569f311284728
jo5huar3 <jrlengineer@outlook.com>
2024-10-06 01:23:02 -0500

    Converted to SPA and added app routes

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppRoutes from './AppRoutes.jsx';
+//import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
+import { Layout } from './components/Layout.jsx';
+//import './custom.css';
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render() {
+    return (
+      <Layout>
+          <Routes>
+            {AppRoutes.map((route, index) => {
+              const { element, ...rest } = route;
+              return <Route key={index}  {...rest} element={element} />;
+            })}
+          </Routes>
+      </Layout>
+    );
+  }
+}
